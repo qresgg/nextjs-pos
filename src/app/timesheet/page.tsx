5fccfa9fae34a7e7602a93f7417f1e0f67f74421
Vitaliy Peteliuk <ernestocarmonegg@gmail.com>
2025-11-01 00:47:37 -0500

    started work on timesheet page 10/31

--- src/app/timesheet/page.tsx
+++ src/app/timesheet/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+import {useUserStore} from "@/store/useUserStore";
+import styles from "./page.module.scss"
+
 export default function Timesheet () {
+    const { user } = useUserStore();
   return (
-    <div>
-      TIMESHEET
+    <div className={styles.clockPage}>
+        <div className={styles.leftPanel}>
+            <div className={styles.header}>Timesheet</div>
+            <div className={styles.logo}>
+                <div className={styles.logoImg}>
+                    LOGO
+                </div>
+            </div>
+            <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <div className={styles.fullName}>{user?.firstName} {user?.lastName}</div>
+                    <div className={styles.role}>{user?.role}</div>
+                </div>
+            </div>
+        </div>
+        <div className={styles.rightPanel}>
+            1
+        </div>
     </div>
   );
 };
